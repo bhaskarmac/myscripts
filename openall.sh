34fa3cf4a3f54a73ce87e4ae8a6f5b20afcd4eef
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-07-27 12:58:22 +0530

    Added openall.sh script - which opens the mentioned sites and applications

---
+++ openall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -
+#title           :openall.sh
+#description     :This script will open all the websites or application which you provide in the list
+#author		       :Bhaskar
+#version         :0.1
+#usage		       :bash openall.sh
+#bash_version    :4.3.8(1)-release
+#==============================================================================
+python -m webbrowser "https://www.google.co.in/"
+python -m webbrowser "https://mail.google.com/"
+python -m webbrowser "https://www.smashingmagazine.com/"
+python -m webbrowser "https://css-tricks.com/"
+python -m webbrowser "https://en.todoist.com/"
+atom
